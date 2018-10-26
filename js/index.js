a64be5084cc6074f66124a09052b5529b8fb0124
Jun Lee <jun@Juns-MacBook-Air.local>
2018-10-26 13:37:29 -0700

    added confirm screen, moved stuff around

--- js/index.js
+++ js/index.js
@@ -1,26 +1,48 @@
-var sampleID = "bob";
-var samplePwd = "password";
+const sampleID = "bob";
+const samplePwd = "password";
 
+var loggedIn = false;
+
+<!-- logging in with account -->
 $("#login-btn").click(login);
 function login(e){
     var id = $("#userID").val();
     var pwd = $("#userPassword").val();
 
-    if(id == sampleID && pwd == samplePwd)
+    if(id === sampleID && pwd === samplePwd) {
+        loggedIn = true;
         window.location.replace("activity.html");
-    else
+    }else {
         alert("account not found");
+    }
     e.preventDefault();
 }
 
+<!-- for joining an existing poll. -->
 $("#join-btn").click(join);
 function join(e){
-
     e.preventDefault();
 }
 
+<!-- guest poll creation -->
 $("#guest-btn").click(guest);
 function guest(e){
+    loggedIn = false;
     window.location.replace("activity.html");
+    e.preventDefault();
+}
+
+$("#add-activities").click(findActivities);
+
+function findActivities(e){
+    alert("looking for activites");
+}
+
+$("#back").click(backToHome);
+function backToHome(e){
+    if(confirm("are you sure you want to exit?")){
+        loggedIn = false;
+        window.location.replace("index.html");
+    }
     e.preventDefault();
 }
