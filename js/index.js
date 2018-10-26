bc848eb43066e26e8e3d52ea012f8f5161a00054
Jun Lee <jun@Juns-MacBook-Air.local>
2018-10-26 13:21:29 -0700

    basic activities screen

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-$("#login-btn").click(login);
+var sampleID = "bob";
+var samplePwd = "password";
 
-function login(event){
-    
+$("#login-btn").click(login);
+function login(e){
+    var id = $("#userID").val();
+    var pwd = $("#userPassword").val();
+
+    if(id == sampleID && pwd == samplePwd)
+        window.location.replace("activity.html");
+    else
+        alert("account not found");
+    e.preventDefault();
+}
+
+$("#join-btn").click(join);
+function join(e){
+
+    e.preventDefault();
+}
+
+$("#guest-btn").click(guest);
+function guest(e){
+    window.location.replace("activity.html");
+    e.preventDefault();
 }
