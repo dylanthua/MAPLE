affbf55977502a65eb06493edfcf905b2ed37e3f
Jun Lee <jun@Juns-MacBook-Pro.local>
2018-10-31 11:46:18 -0700

    all pages in place

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ var sampleID = "bob";
 var samplePwd = "password";
 
 var loggedIn = false;
+var creator = true;
+var activities = [];
 
 <!-- logging in with account -->
 $("#login-btn").click(login);
@@ -10,6 +12,7 @@ function login(e){
     var pwd = $("#userPassword").val();
 
     if(id === sampleID && pwd === samplePwd) {
+        creator = true;
         loggedIn = true;
         window.location.replace("activity.html");
     }else {
@@ -26,6 +29,7 @@ function join(e){
     if(code == null || code === ""){
     }else{
         if(code === pollCode){
+            creator = false;
             window.location.replace(code+".html");
         }else{
             alert("POLL NOT FOUND!");
@@ -38,22 +42,19 @@ function join(e){
 $("#guest-btn").click(guest);
 function guest(e){
     loggedIn = false;
+    creator = true;
     window.location.replace("activity.html");
     e.preventDefault();
 }
 
+<!-- signup will add modal later -->
 $("#signup").click(signup);
 function signup(e){
 
     e.preventDefault();
 }
 
-<!-- activities.html stuff -->
-$("#add-activities").click(findActivities);
-function findActivities(e){
-    alert("looking for activites");
-}
-
+<!-- activities.html -->
 $("#back").click(backToHome);
 function backToHome(e){
     if(confirm("are you sure you want to exit?")){
@@ -63,11 +64,47 @@ function backToHome(e){
     e.preventDefault();
 }
 
+$("#add-activities").click(function (e){
+    window.location.replace("find_activities.html");
+    e.preventDefault();
+});
+
+$("#start-poll").click(function (e){
+    window.location.replace("1a234b.html");
+    e.preventDefault();
+});
+
 
 <!-- for voting in a poll -->
 $("#submit_vote").click(function(e){
-   alert("VOTE SUBMITTED!");
+    if(creator){
+        window.location.replace("results.html");
+    }else {
+        alert("VOTE SUBMITTED!");
+        window.location.replace("index.html");
+    }
+    e.preventDefault();
+});
 
-   window.location.replace("index.html");
-   e.preventDefault();
+
+<!-- for finding activities -->
+$("#back_to_activities").click(function (e){
+    window.location.replace("activity.html");
+    e.preventDefault();
+});
+
+$("#refresh").click(function (e){
+   alert("searching...");
+});
+
+$("#done-searching").click(function (e){
+    window.location.replace("activity.html");
+
+    e.preventDefault();
+});
+
+<!-- last page -->
+$("#finish-poll").click(function (e){
+    window.location.replace("index.html");
+    e.preventDefault();
 });
